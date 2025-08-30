c90a8569d8b5ea6e9071b9cc6845139388e30a92
Pooja-hue7 <bochare532@gmail.com>
2025-08-30 18:43:19 +0530

    Add files via upload

---
+++ operators.py
@@ -0,0 +1,86 @@
+# '''  OPERATORS are special symbol that are used in python there are some types
+# !)arithmatical operator
+# 1) + operators = + OPERATORS are used to add two no. it present addition
+
+# '''
+# print(10+20)
+# print('hello+user')
+# l1=[11,22]
+# l2=[33,44]
+# print(l1+l2)
+# # dict={'name':'pooja','age':20}
+# # dict2={'name':'neha','age':21}
+# # print(dict+dict2)
+# n=12
+# n1=20
+# print(n+n1)
+# print(n.__add__(n1))
+
+# '''
+# # / operator =always return float value
+# '''
+
+
+# '''
+# comparision operator
+# '''
+# n1=5
+# n2=4
+# print(n1<n2)
+
+# '''
+# assignment operator
+# 1) +=
+# 2)-=
+# 3)%=
+# '''
+# n1=10
+# n1-=4
+# print(n1)
+
+# n1=10
+# n1+=4
+# print(n1)
+
+# num=10
+# num%=2
+# print(num)
+
+# '''
+# membership operators
+# 1)in
+# 2)not in
+# '''
+# numbers=[11,22,33,44,55]
+# print(33 in numbers)
+# print(444 not in numbers)
+
+# '''
+# identity operator
+# 1)is operator
+# 2)is not operator
+# '''
+# n1=100
+# n2=100
+# print(n1 is n2)
+
+# l1=[10,20]
+# l2=[10,20]
+# print(l1 is l2)
+
+# l1=[10,20]
+# l2=[10,20]
+# print(l1 is not l2)
+
+# '''
+# logical operator
+# 1)
+# 2)
+# 3)
+# 4)
+# '''
+# print([1, 2] is [1, 2])
+# print(8 >> 2)
+# print(5 is 5)
+
+print(-10//3)   #-4   return small value
